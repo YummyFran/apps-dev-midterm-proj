fe25fccd80bdc20ac955f742e81146992b26b9b6
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-10-03 22:25:22 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
